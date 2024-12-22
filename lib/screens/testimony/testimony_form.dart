e57cc44a1ea814124d08701602176a1ba9ff0887
DaoistXuandu <akbarraihan2005@gmail.com>
2024-12-22 14:09:08 +0700

    fix runtime

--- lib/screens/testimony/testimony_form.dart
+++ lib/screens/testimony/testimony_form.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
 import 'package:bandung_couture_mobile/models/testimony/testimony.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -144,48 +142,31 @@ class _TestimonyFormPageState extends State<TestimonyformPage> {
                           ? '${URL.urlLink}testimony/edit_testimony_flutter/'
                           : '${URL.urlLink}testimony/add_testimony_flutter/';
 
-                      try {
-                        final response = await request.postJson(
-                          url,
-                          jsonEncode(<String, String>{
-                            'testimony': _testimony,
-                            'rating': _rating.toString(),
-                            'store_id': widget.id.toString(),
-                            if (widget.edit)
-                              'pk': widget.instance!.pk.toString()
-                          }),
-                        );
-                        if (context.mounted) {
-                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text(
-                                '${response['status']}: ${response['message']}'),
-                          ));
-                          print("test-[1]");
-                          if (response['status'] == 'success') {
-                            Navigator.pop(context);
-                            // Navigator.push(
-                            //     context,
-                            //     MaterialPageRoute(
-                            //         builder: (context) => TestimonyMerchantPage(
-                            //               storeName: widget.storeName,
-                            //               description: widget.description,
-                            //               storeId: widget.id,
-                            //             )));
-                          }
+                      final response = await request.postJson(
+                        url,
+                        jsonEncode(<String, String>{
+                          'testimony': _testimony,
+                          'rating': _rating.toString(),
+                          'store_id': widget.id.toString(),
+                          if (widget.edit) 'pk': widget.instance!.pk.toString()
+                        }),
+                      );
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                          content: Text(
+                              '${response['status']}: ${response['message']}'),
+                        ));
+                        if (response['status'] == 'success') {
+                          Navigator.pop(context);
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: (context) => TestimonyMerchantPage(
+                          //               storeName: widget.storeName,
+                          //               description: widget.description,
+                          //               storeId: widget.id,
+                          //             )));
                         }
-                      } on TimeoutException {
-                        // Handle timeout specifically
-                        print(
-                            'Request timed out. Please check your internet connection');
-                      } on SocketException {
-                        // Handle no internet connection
-                        print('No internet connection');
-                      } on HttpException catch (e) {
-                        // Handle HTTP errors
-                        print('HTTP error occurred: ${e.message}');
-                      } catch (e) {
-                        // Handle other errors
-                        print('Error occurred: $e');
                       }
                     }
                   },
